f58b2b558b7a673d51c54f1f274b27a02bbc03f2
omaimaelbaz <elbazouma70@gmail.com>
2023-09-14 02:45:50 +0100

    challenge 6 et 7

---
+++ challenge6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main ( ) {
+    float r ;
+    float circonference ;
+    printf("ernter le rayon :");
+    scanf("%f" , &r);
+    printf (" trouver la circonference d'un cercle !! \n");
+    circonference = 2 *3.14 *r ;
+    printf (" la circonference du cercle est : %f ",circonference);
+
+
+
+}
